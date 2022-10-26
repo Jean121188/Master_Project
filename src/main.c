f5adf907f6e0e964335bb1b36dfe65134f7e15c0
unknown <jeandesouzaprograming@gmail.com>
2022-10-26 18:20:42 -0300

    Teste LOG_BUFFER

--- src/main.c
+++ src/main.c
@@ -59,11 +59,13 @@ static void rx_task()
     esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
     char* data = (char*)malloc(RX_BUF_SIZE+1);
     while (1) {
-        const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_RATE_MS);
+        // const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_RATE_MS);
+        const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, portMAX_DELAY);
         if (rxBytes > 0) {
             data[rxBytes] = 0;
             ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
             //ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+            // ESP_LOG_BUFFER_CHAR(RX_TASK_TAG, data, rxBytes);
         }
     }
     free(data);
